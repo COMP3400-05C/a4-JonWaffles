eb75a51e557c5a76252345401e9a5f57abf20e55
Jonathan Hua-Phan <144959315+JonWaffles@users.noreply.github.com>
2025-10-31 17:13:20 -0400

    Update parta.c

--- parta.c
+++ parta.c
@@ -1,8 +1,26 @@
 #include <stdbool.h>
 #include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <ctype.h>
+#include <stdio.h>
 
 int main(int argc, const char* argv[]) {
+    if (argc <= 1) {
+        printf("ERROR: No arguments");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        if (i > 1) {
+            putchar(',');
+        }
+
+        const char* current = argv[i];
+        while (*current != '\0') {
+            unsigned char ch = (unsigned char)(*current);
+            putchar(toupper(ch));
+            ++current;
+        }
+    }
+
     return 0;
 }
